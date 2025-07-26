54b2e6a65f2f86f351ab56338f5a65a716c9861d
Fantasy <1362724990@qq.com>
2025-07-27 02:12:47 +0800

    feat: 用户模块

--- src/request.ts
+++ src/request.ts
@@ -22,7 +22,8 @@ myAxios.interceptors.response.use(function (response) {
   // 未登录
   if (data.code === 40100) {
     // 不是获取用户信息的请求，并且用户目前不是已经在用户登录页面，则跳转到登录页面
-    if (!response.request.responseURL.include('/user/get/login') && !window.location.pathname.includes('/user/login')) {
+    if (!response.request.responseURL.includes('/user/get/login')
+      && !window.location.pathname.includes('/user/login')) {
       message.warning('请先登录');
       window.location.href = `/user/login?redirect=${window.location.href}`;
     }
